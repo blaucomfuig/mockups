128028e0135831d874dc096589c254a5fb318fd1
blaucomfuig <sergio.corcuera01@estudiant.upf.edu>
2020-12-15 23:20:06 +0100

    añadir scroll

---
+++ docs/scroll.js
@@ -0,0 +1,69 @@
+
+ScrollReveal().reveal('.big-title', {
+    delay: 700,
+    duration: 2000,
+    origin: "top",
+    distance: "400px",
+    easing: 'ease'
+})
+
+ScrollReveal().reveal('.about-us-title', {
+    delay: 600,
+    duration: 1500,
+    origin: "top",
+    distance: "200px",
+    easing: 'ease'
+})
+
+ScrollReveal().reveal('.grid-about', {
+    delay: 800,
+    duration: 1500,
+    origin: "top",
+    distance: "200px",
+    easing: 'ease'
+})
+
+ScrollReveal().reveal('.categories-title', {
+    delay: 400,
+    duration: 1500,
+    origin: "top",
+    distance: "200px",
+    easing: 'ease'
+})
+
+ScrollReveal().reveal('.grid-categories', {
+    delay: 700,
+    duration: 1500,
+    origin: "top",
+    distance: "200px",
+    easing: 'ease'
+})
+
+ScrollReveal().reveal('.grid-categories-item-1', {
+    delay: 1000,
+    duration: 1500,
+    origin: "left",
+    distance: "200px",
+    easing: 'ease'
+})
+ScrollReveal().reveal('.grid-categories-item-2', {
+    delay: 1500,
+    duration: 1500,
+    origin: "left",
+    distance: "200px",
+    easing: 'ease'
+})
+ScrollReveal().reveal('.grid-categories-item-3', {
+    delay: 2000,
+    duration: 1500,
+    origin: "left",
+    distance: "200px",
+    easing: 'ease'
+})
+ScrollReveal().reveal('.grid-categories-item-4', {
+    delay: 2500,
+    duration: 1500,
+    origin: "left",
+    distance: "200px",
+    easing: 'ease'
+})
